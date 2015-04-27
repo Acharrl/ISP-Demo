673e14a6180f0e367a88e2d332f7d86512b806ed
Acharrl <andrewmazof@yahoo.com>
2015-04-27 17:29:43 -0400

    Anything

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+	[RequireComponent (typeof (CharacterController))]
+public class PlayerController : MonoBehaviour {
+
+	//Handling
+	public float rotationSpeed = 450;
+	public float walkSpeed = 5;
+	public float runSpeed = 8;
+
+	//System
+	private Quaternion targetRotation;
+
+	//Components
+	public Gun gun;
+	private CharacterController controller;
+	private Camera cam;
+
+	
+	void Start()
+	{
+		controller = GetComponent<CharacterController>();
+		cam = Camera.main;
+	}
+
+	void Update()
+	{
+		ControlMouse ();
+		//ControlWASD():
+
+		if (Input.GetButtonDown ("Shoot")) {
+			gun.Shoot ();
+		} else if (Input.GetButton ("Shoot")) {
+			gun.ShootContinuous();
+		}
+
+	}
+
+	void ControlMouse(){
+
+		Vector3 mousePos = Input.mousePosition;
+		mousePos = cam.ScreenToWorldPoint (new Vector3 (mousePos.x, mousePos.y, cam.transform.position.y - transform.position.y));
+		targetRotation = Quaternion.LookRotation(mousePos - new Vector3(transform.position.x,0,transform.position.z));
+		transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle (transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
+
+		Vector3 input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+		//No acceleration for .GetAxisRaw compared to .GetAxis		
+		Vector3 motion = input;
+		motion *= (Mathf.Abs (input.x) == 1 && Mathf.Abs (input.z) == 1) ? .7f : 1;
+		motion *= (Input.GetButton ("Run")) ? runSpeed : walkSpeed;
+		motion += Vector3.up * -8;
+		
+		controller.Move (motion * Time.deltaTime);
+	}
+
+	void ControlWASD(){
+		Vector3 input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+		//No acceleration for .GetAxisRaw compared to .GetAxis
+		
+		
+		if (input != Vector3.zero) { //So that it doesn't snap back to forward
+			targetRotation = Quaternion.LookRotation (input);
+			//Makes player look in direction of travel
+			transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y,targetRotation.eulerAngles.y,rotationSpeed * Time.deltaTime);
+		}
+		
+		Vector3 motion = input;
+		motion *= (Mathf.Abs (input.x) == 1 && Mathf.Abs (input.z) == 1) ? .7f : 1;
+		motion *= (Input.GetButton("Run"))?runSpeed:walkSpeed;
+		motion += Vector3.up * -8;
+		
+		controller.Move(motion * Time.deltaTime);
+	}
+}
