9d90d263167a011e292b30e78d73b8dccc941e97
Nathan Wolfe <nathanwolferl@gmail.com>
2015-04-29 10:09:26 -0400

    added navmesh

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+	public Transform goal;
+
+	private NavMeshAgent agent;
+
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		agent.destination = goal.position;
+	}
+}
