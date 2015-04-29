22fce8e55b42b157ecef597081e3f729bf33b544
Nathan Wolfe <nathanwolferl@gmail.com>
2015-04-29 10:26:41 -0400

    added rudimentary enemy, no hearing yet

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour
 {
-	public Transform goal;
+	public Transform reactor;
+	public float fovAngle;
+	public float fovRange;
 
 	private NavMeshAgent agent;
 
 	void Start()
 	{
 		agent = GetComponent<NavMeshAgent>();
-		agent.destination = goal.position;
+		agent.destination = reactor.position;
+	}
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			Vector3 direction = other.transform.position - transform.position;
+			float angle = Vector3.Angle(direction, transform.forward);
+			if(angle < fovAngle / 2)
+			{
+				RaycastHit hit;
+				if(Physics.Raycast(transform.position, direction.normalized, out hit, fovRange))
+				{
+					if(hit.collider.gameObject.tag == "Player")
+					{
+						agent.destination = other.gameObject.transform.position;
+					}
+				}
+			}
+		}
 	}
 }
