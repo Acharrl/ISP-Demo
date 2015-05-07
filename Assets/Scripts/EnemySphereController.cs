8fca6ad7d1acf738fb803f56dceec2a9605282b7
Nathan Wolfe <nathanwolferl@gmail.com>
2015-05-07 09:59:44 -0400

    added in enemy cloud thing

---
+++ Assets/Scripts/EnemySphereController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySphereController : MonoBehaviour
+{
+	private Transform sphere;
+	private float rotationSpeed;
+	private Vector3 originalPos;
+	private float flinchRadius = 1;
+
+	void Start()
+	{
+		sphere = transform.GetChild(0);
+		originalPos = Random.Range(0.25f, 0.5f) * Vector3.forward;
+		sphere.localPosition = originalPos;
+		transform.rotation = Quaternion.Euler(new Vector3(Random.Range(0, 360), Random.Range(0, 360), 0));
+		rotationSpeed = Random.Range(180, 360);
+	}
+
+	void Update()
+	{
+		transform.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime, 0));
+		if(flinchRadius - 1 > Time.deltaTime)
+		{
+			flinchRadius -= Time.deltaTime;
+		}
+		else
+		{
+			flinchRadius = 1;
+		}
+		sphere.localPosition = flinchRadius * originalPos;
+	}
+
+	public void Flinch()
+	{
+		flinchRadius = 2;
+	}
+}
